08b4821c36ce08f9477b7772454f826c3612dcfa
Tair <tairjanametov@gmail.com>
2024-08-10 20:26:28 +0500

    train with types and interfaces

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 function meanVal(a, b) {
     console.log(a + b);
 }
-meanVal(3, 3);
+meanVal(3, '3');
+var newYear = 1900;
+var num = 3;
+var programmer = {
+    id: 2,
+    name: 'Air',
+    role: 'Front',
+    birthDay: 24,
+    birthYear: 2000,
+    hasHobby: true,
+    getData: function (name, role) {
+        console.log(name + " has role: " + role);
+    }
+};
+programmer.getData(programmer.name, programmer.role);
--- index.ts
+++ index.ts
@@ -1,5 +1,31 @@
-function meanVal(a: number, b: number) {
+function meanVal(a: number, b: any) {
     console.log(a + b)
 
 }
-meanVal(3, 3)
+meanVal(3, '3')
+let newYear: number = 1900
+
+type f = string | number
+
+let num: f = 3
+interface user {
+    readonly id: number
+    name: string
+    role: string
+    birthDay?: number,
+    birthYear?: number,
+    hasHobby: boolean,
+    getData: (name: string, role: string) => void
+}
+const programmer: user = {
+    id: 2,
+    name: 'Air',
+    role: 'Front',
+    birthDay: 24,
+    birthYear: 2000,
+    hasHobby: true,
+    getData: (name, role) => {
+        console.log(name + " has role: " + role)
+    }
+}
+programmer.getData(programmer.name, programmer.role)
